257152d921fc6a0cf52ae8d2c8f0a36fc6623e49
SzThomas <szente_tamas@yahoo.com>
2018-03-13 21:05:10 +0200

    RateMe13/02

--- RateMe/RateMe.Core/Properties/AssemblyInfo.cs
+++ RateMe/RateMe.Core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("be306dba-b70a-4af7-ab69-e6d759b50fea")]
+[assembly: Guid("18f9c999-d658-401d-8f48-2e2f35555116")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ RateMe/RateMe.UI/Controllers/MovieController.cs
@@ -0,0 +1,101 @@
+﻿using RateMe.BLL;
+using RateMe.Core.Models;
+using RateMe.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RateMe.UI.Controllers
+{
+    public class MovieController : Controller
+    {
+        private MovieService ms = new MovieService(new MovieDataRepository());
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            List<Movie> movies =  ms.GetAllMovies();
+            return View(movies);
+        }
+
+        // GET: Movie/Details/5
+        public ActionResult Details(int id)
+        {
+            
+            return View();
+        }
+
+        // GET: Movie/Create
+        public ActionResult Create()
+        {
+                
+            return View();
+        }
+
+        // POST: Movie/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Movie m = ms.GetMovieById(id);
+            return View(m);
+        }
+
+        // POST: Movie/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Movie collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                ms.Save(collection);
+                return RedirectToAction("Index");
+            }
+            catch(Exception exception)
+            {
+                var a = exception.Message;
+                return View();
+            }
+        }
+
+        // GET: Movie/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Movie m = ms.GetMovieById(id);
+            return View();
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Movie collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                ms.Delete(id);
+                return RedirectToAction("Index");
+            }
+            catch (Exception exception)
+            {
+                var a = exception.Message;
+                return View();
+            }
+        }
+    }
+}
